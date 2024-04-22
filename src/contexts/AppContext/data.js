a8921318eb4a4850524abe52a9e205d4e7df8e65
Júlio António <julioantonio1408@gmail.com>
2024-04-22 14:44:29 +0100

    useContext segundo

---
+++ src/contexts/AppContext/data.js
@@ -0,0 +1,5 @@
+export default{
+    title: 'O título que contexto',
+    body: 'O body do contexto',
+    counter: 0,
+  };
---
+++ src/contexts/AppContext/index.jsx
@@ -0,0 +1,10 @@
+import { createContext, useContext, useState} from 'react';
+import globalState from './data';
+
+export const GlobalContext = createContext();
+
+// eslint-disable-next-line
+export const AppContext = ({children}) => {
+    const [contextState, setState] = useState(globalState);
+    return (<GlobalContext.Provider value={{contextState,setState}}>{children}</GlobalContext.Provider>);
+}
