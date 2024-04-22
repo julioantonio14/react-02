297e7127a32ce850f939eb9dbbdb482af61a1a1c
Júlio António <julioantonio1408@gmail.com>
2024-04-22 10:38:38 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -1,45 +1,78 @@
-import logo from './logo.svg';
+import P from 'prop-types';
+import { useEffect, useMemo, useState, useRef } from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
 
-const eventFn = () => {
-    console.log('h1 clicado');
+const Post = ({ post, handleClick }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+  handleClick: P.func,
 };
 
 function App() {
-    const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
-    //componetDidUpdate = executa toda vez que o component atualiza
-    useEffect(() => {
-        document.querySelector('h1')?.addEventListener('click', eventFn);
-        console.log('componetDidUpdate');
-    });
+  console.log('Pai renderizou!');
 
-    //componetDidMount = executa 1x
-    let didInit = false;
-    useEffect(() => {
-        if (!didInit) {
-            didInit = true;
-            console.log('componetDidMount');
-        }
-    }, []);
-    useEffect(() => {
-        return () => {
-            document.querySelector('h1')?.removeEventListener('click', eventFn);
-        };
-    }, []);
+  // Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+  }, []);
 
-    //Com dependência - executa toda vez que a dependência mudar
-    useEffect(() => {
-        console.log('Counter mudou para', counter);
-    }, [counter]);
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
 
-    return (
-        <div className="App">
-            <h1>Contadora: {counter}</h1>
-            <button onClick={() => setCounter(counter + 1)}>+</button>
-        </div>
-    );
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="App">
+      <h6>Renderizou: {contador.current}x</h6>
+      <p>
+        <input
+          ref={input}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
+    </div>
+  );
 }
 
 export default App;
